0c389e124a27c89a2d9cf94ed342479384f81c5d
Kartikay <sky.kartikay@gmail>
2023-12-22 20:01:36 +0530

    triak003.4

---
+++ src/app/(root)/teams/page.js
@@ -0,0 +1,9 @@
+
+export default function Page(){
+    return (
+        <div className="mx-4 my-8">
+          <h2 className="text-2xl font-bold mb-4">Teams</h2>
+          {/* <TeamSlider teams={teams} handleTeamClick={handleTeamClick} selectedTeam={selectedTeam} /> */}
+        </div>
+    );
+}
