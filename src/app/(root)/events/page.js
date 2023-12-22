db6f415d66c1ce3c2b39309105ed2cc13f6ebe82
Kartikay <sky.kartikay@gmail>
2023-12-22 19:10:28 +0530

    trial 003

--- src/app/(root)/events/page.js
+++ src/app/(root)/events/page.js
@@ -1,7 +1,30 @@
 import Nav from "@/components/Nav";
+import Zhome from "@/components/Zhome";
+import EventCard from "@/components/EventCard";
+import Navbar from "@/components/Navbar";
+import ProgramCard from "@/components/ProgramCard";
+import Rest from "@/components/Rest";
+import TeamSlider from "@/components/TeamSlider";
+
+const teams = [
+    { name: 'Tech', slug: 'tech' },
+    { name: 'Core', slug: 'core' },
+    { name: 'Event Management', slug: 'event-management' },
+    { name: 'PR', slug: 'pr' },
+    { name: 'Sponsorship', slug: 'sponsorship' },
+    { name: 'Content', slug: 'content' },
+    { name: 'Logistics', slug: 'logistics' },
+    { name: 'Leads', slug: 'leads' },
+  ];
+
 export default function Page (){
     return(<>
-    
+        <Zhome/>
+        <EventCard/>
+        <Navbar/>
+        <ProgramCard/>
+        <Rest/>
+        {/* <TeamSlider teams={teams}/> */}
         <Nav></Nav>
         <Nav></Nav>
         </>
